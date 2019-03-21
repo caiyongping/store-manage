5003410a4a4a65b300679fca3113eafb9239e75f
caiyongping <877607901@qq.com>
2019-03-21 22:17:17 +0800

    springboot

---
+++ src/main/java/com/oyo/storemanage/StoreManageApplication.java
@@ -0,0 +1,13 @@
+package com.oyo.storemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oyo/storemanage/StoreManageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oyo.storemanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreManageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
